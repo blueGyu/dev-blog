f770cbbff4e6afc259e7b348e606a67415cbfc25
blueGyu <bluegyufordev@gmail.com>
2025-02-24 22:24:45 +0900

    init: Turborepo 기반 프로젝트 초기 설정

---
+++ apps/admin/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="bg-slate-700">블로그 관리 사이트</div>;
+}
---
+++ apps/client/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="bg-slate-500">기술 블로그</div>;
+}
